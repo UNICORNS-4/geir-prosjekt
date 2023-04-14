46275f9dda9738ba958a345a0695c144fe5a9d0e
Philip Olafsen <ph.olafsen@gmail.com>
2023-04-14 10:16:51 +0200

    feat: added new dashboard folder for work on it.

---
+++ .Prosjekt/Model/dashboard/dashboard.js
@@ -0,0 +1,20 @@
+function updateDashboard(){
+    let html = /*HTML*/`
+    <div class="navBar dashboard">
+        <div class="navBarItem">Hjem</div>
+        <div class="navBarItem">Temaer</div>
+        <div class="navBarItem">Meldinger</div>
+        <div class="navBarItem navBarUser">${model.app.loggedInUser}</div>
+    </div>
+    `;
+return html;
+}
+
+function findUser(userID) {
+    for (let user of model.users) {
+      if (user.userId == userID) {
+        return user;
+      }
+    }
+    return null;
+  }
