b9eb7fff995d4f139c3a0d03ed4bd45a021aa88d
Philip Olafsen <ph.olafsen@gmail.com>
2023-04-14 12:51:59 +0200

    fix

--- .Prosjekt/Model/dashboard/dashboard.js
+++ .Prosjekt/Model/dashboard/dashboard.js
@@ -4,8 +4,12 @@ function updateDashboard(){
         <div class="navBarItem">Hjem</div>
         <div class="navBarItem">Temaer</div>
         <div class="navBarItem">Meldinger</div>
-        <div class="navBarUser">${model.app.loggedInUser}</div>
-    </div>
+        <div>
+          <div class="navBarUser">${findUser(model.app.loggedInUser).firstname} ${findUser(model.app.loggedInUser).lastname}</div>
+          <div class="navBarUserClassID">${findUser(model.app.loggedInUser).klasse}</div>
+        </div>
+    
+    </div>-
     `;
 return html;
 }
