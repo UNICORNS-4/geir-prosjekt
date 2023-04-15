6b2a210b1cef21739fd60647a00c6a081d45c4c8
SebastianWaage <sebastian.waage98@hotmail.com>
2023-04-15 02:00:23 +0200

    progSortering side og ProfilePage

---
+++ .Prosjekt/Model/ProfilePage/ProfilepageView.js
@@ -0,0 +1,25 @@
+
+document.getElementById('app').innerHTML = updateProfilepageView();
+
+function updateProfilepageView() {
+  let html = /* Html */ `
+  <div id="ProfilePic" ><img id="img1" style="width:150px" src="img/img1.jpeg"></div>
+  <div id="ProfileName">@SuperStudent</div>
+  <div id="StartIT">START IT</div>
+  <div id="Progressbars"></div>
+  <div id="Bars">${updateBars()}</div>
+  `;
+  return html;
+}
+
+function updateBars(){
+  let html = `
+  <label for="bar1">Tema 1:</label>
+  <progress id = "bar1" value="23" max="100"></progress>
+  <label for="bar2">Tema 2:</label>
+  <progress id = "bar2" value="12" max="100"></progress>
+  <label for="bar3">Tema 3:</label>
+  <progress id = "bar3" value="96" max="100"></progress>
+`
+  return html;
+}
---
+++ .Prosjekt/Model/Progresjonsortering/ProgsortView.js
@@ -0,0 +1,8 @@
+document.getElementById('app').innerHTML = ProgressionListBars();
+
+
+function ProgressionListBars() {
+    for (let i = 0; i < model.users.length; i++){html += `
+    <label for="bar${i}">@Arne</label>
+    <progress id ="bar${i}" value="0" max"300"></progress>`
+}}
