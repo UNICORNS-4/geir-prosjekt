2751827a905573e9bfbf6227826d7e0b5ec006cb
Philip Olafsen <ph.olafsen@gmail.com>
2023-04-23 16:37:56 +0200

    feat: added styles to the login page

--- .Prosjekt/Model/workingstuff/LoginPageView.js
+++ .Prosjekt/Model/workingstuff/LoginPageView.js
@@ -4,25 +4,30 @@ function updateViewLoginPage() {
     model.inputs.loginPage.userId = "";
     model.inputs.loginPage.password = "";
     console.log("#loginPageView_UPDATED")
-  return /*HTML*/ `
-        Brukernavn:<br/>
+    return /*HTML*/ `
+<div id ="loginPageView"> 
         <input 
+        class="loginPageInputs" 
+
+            placeholder="brukernavn"
             type="text" 
             value="${model.inputs.loginPage.userId || ""}"
             oninput="model.inputs.loginPage.userId = this.value"
             /><br/>
-        Passord:<br/>
+
+
         <input 
+            placeholder="passord"
+            class="loginPageInputs" 
             type="password" 
             value="${model.inputs.loginPage.password || ""}"
             oninput="model.inputs.loginPage.password = this.value"
             /><br/>
 
+
         <div style="color:red">
             ${model.inputs.loginPage.errorMessage || ""}
         </div>
-        <button onclick="logIn()">Logg inn</button>
-    
-    <button onclick="updateBarView()">Progressbars</button>
-        `;
+        <button class="loginPageBtns" onclick="logIn()">Logg inn</button>
+`;
 }
