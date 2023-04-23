e4c6da02f314650e6a8986977ddfba5df791089f
Philip Olafsen <ph.olafsen@gmail.com>
2023-04-23 13:30:07 +0200

    feat: updates to dashboard.js and css for it.

--- .Prosjekt/Model/workingstuff/dashboard.js
+++ .Prosjekt/Model/workingstuff/dashboard.js
@@ -28,7 +28,7 @@ function updateDashboard() {
                   <div class="navBarUser">${model.app.loggedInUser.firstname} ${model.app.loggedInUser.lastname}</div>
                   <div class="navBarUserClassIDAdmin">${model.app.loggedInUser.klasse}</div>
               </div>
-    <div class="navBarItem logOutBtn" onclick="logOut()"> ${loggOutSvg}</div>
+    <div class="navBarItem logOutBtn" onclick="logOut()">Log ut${loggOutSvg}</div>
     </div>`;
     return html;
 
