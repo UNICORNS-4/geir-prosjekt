c4334c178b3b0ebb5f5daac25b9c64eec166c22e
MarlieJ <122234826+MarlieJ@users.noreply.github.com>
2023-04-24 22:31:36 +0200

    Tatt kopi av workstuff til marleneLegger til tema

---
+++ .Prosjekt/Model/MarleneLeggerTilTema/View.js
@@ -0,0 +1,31 @@
+const app=document.getElementById("app");
+
+
+
+
+function updateView(){
+    switch(model.app.currentPage){
+        case"frontPage":
+            app.innerHTML= updateDashboard();
+            break;
+        case"loginPage":
+            app.innerHTML= updateViewLoginPage();
+            break;
+        case"logPage":
+            app.innerHTML= updateDashboard() + updateViewLogPage();
+            break;
+        case"profilePage":
+            app.innerHTML= updateDashboard() + updateProfilepageView();
+            break;
+        case"newUserPage":
+            app.innerHTML= updateDashboard() + updateViewNewUserPage();
+            break;
+        case"topicPage":
+            app.innerHTML= updateDashboard() + updateViewTopicPage();
+            break;
+        case"elevListePage":
+            app.innerHTML= updateDashboard() + updateElevListeView();
+            break;
+    }
+}
+window.onload = updateView();
