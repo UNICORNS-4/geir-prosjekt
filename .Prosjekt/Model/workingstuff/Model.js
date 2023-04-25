9b250f1c1b87148738924dd63bd578a955a5029c
MarlieJ <122234826+MarlieJ@users.noreply.github.com>
2023-04-25 20:11:55 +0200

    Endret bildenavn, små endringer

--- .Prosjekt/Model/workingstuff/Model.js
+++ .Prosjekt/Model/workingstuff/Model.js
@@ -1,12 +1,6 @@
 const model = {
   app: {
-<<<<<<< HEAD
-
-
     currentPage: "profilePage",
-=======
-    currentPage: "topicPage",
->>>>>>> 2d9fefb5a9abce6ef4ff05a76cb62474f1c9f28f
     isAdmin: true,
     loggedInUser: false,
   },
