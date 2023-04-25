891c198b5a48f7b39e6669af698005b97cd5b9b9
MarlieJ <122234826+MarlieJ@users.noreply.github.com>
2023-04-25 16:28:10 +0200

    Filer til backup

---
+++ .Prosjekt/backupMarlene25.04/Model/LoggPageFåDetTilÅFunke/logPageController.js
@@ -0,0 +1,33 @@
+function logPageSelectWeek(weekNo) {
+  model.inputs.logPage.selectedWeek = weekNo;
+  updateView();
+}
+
+function logPageSelectDay(dayNo) {
+  model.inputs.logPage.selectedDay = dayNo;
+  updateView();
+}
+
+function selectStars(questionIndex, starNo) {
+  model.inputs.logPage.answers[questionIndex] = starNo;
+  updateView();
+}
+
+function saveLog() {
+  let userId = model.app.loggedInUser;
+  let user = findUser(userId);
+
+  model.log.push({
+    whatHaveYouLearnedToday: model.inputs.logPage.whatHaveYouLearnedToday,
+    week: model.inputs.logPage.selectedWeek,
+    day: model.inputs.logPage.selectedDay,
+    answers: [...model.inputs.logPage.answers], // ... = spread operator
+    userId: model.app.loggedInUser,
+    module: user.currentModule,
+  });
+  console.log("#LOG_SAVED");
+  console.log(model.log);
+  // model.app.currentPage = 'showLogs';
+  
+  updateView();
+}
---
+++ .Prosjekt/backupMarlene25.04/Model/LoggPageFåDetTilÅFunke/logPageView.js
@@ -0,0 +1,158 @@
+/* let userIdGlobal = model.app.loggedInUser;
+  let user = findUser(userId); */
+  let loggaInnBruker = model.app.loggedInUser;
+  let brukerLog = model.log;
+  let loggaInnBrukerOgBrukerLog = [...loggaInnBruker, ...brukerLog]
+ //let innloggaBruker = [...,...model.log[0]]
+function updateViewLogPage() {
+  if (model.app.loggedInUser.klasse === "Admin") {
+    //console.log("#LOG_UPDATED")
+    //model.users?
+  
+    let html = /*HTML*/ `
+    
+    ${enElevListe()}
+    
+        <h2>Elev logg</h2>
+        <div>
+            ${createWeeksHtml()}
+        </div>
+        <div>
+            ${createDaysHtml()}
+        </div>
+        <textarea 
+            oninput="model.inputs.logPage.whatHaveYouLearnedToday=this.value"
+            >${model.inputs.logPage.whatHaveYouLearnedToday}</textarea>
+        <div>
+            ${createQuestionsHtml()}
+        </div>
+        <button onclick="saveLog()">Lagre</button>
+    `;
+    return html;
+  } else {
+    let html = /*HTML*/ `
+    
+        <h2>Logg</h2>
+        <div>
+            ${createWeeksHtml()}
+        </div>
+        <div>
+            ${createDaysHtml()}
+        </div>
+        <textarea 
+            oninput="model.inputs.logPage.whatHaveYouLearnedToday=this.value"
+            >${model.inputs.logPage.whatHaveYouLearnedToday}</textarea>
+        <div>
+            ${createQuestionsHtml()}
+        </div>
+        <button onclick="saveLog()">Lagre</button>
+        <div>Tidligere loggføringer
+          <div>${model.app.loggedInUser}</div>
+        </div>
+    `;
+    return html;
+  }
+}
+/* model.log.push({
+  whatHaveYouLearnedToday: model.inputs.logPage.whatHaveYouLearnedToday,
+  week: model.inputs.logPage.selectedWeek,
+  day: model.inputs.logPage.selectedDay,
+  answers: [...model.inputs.logPage.answers], // ... = spread operator
+  userId: model.app.loggedInUser,
+  module: user.currentModule,
+}); */
+
+//spørsmål til mandag: 
+//1. problem med index, folk synes ikke på admin sin loggside
+//
+function enElevListe(){
+
+  let html = "";
+  for (let i = 0; i < model.users.length; i++) {
+    html +=
+    ` <ul onclick="">${model.users[i].firstname}  ${model.users[i].lastname}</ul>`
+  
+  }
+return html;
+}
+
+
+
+
+
+//hvis bruker er admin vis studenter og utfylt logg
+//hvis bruker er elev vis loggside og current loggutfylling
+function createWeeksHtml() {
+  let html = "";
+  for (let weekNo = 1; weekNo < 9; weekNo++) {
+    let style =
+      model.inputs.logPage.selectedWeek == weekNo
+        ? "background-color: orange"
+        : "";
+    html += /*HTML*/ `
+            <button 
+                onclick="logPageSelectWeek(${weekNo})"
+                style="${style}"
+                >
+                Uke ${weekNo}
+            </button>
+        `;
+  }
+  return html;
+}
+
+function createDaysHtml() {
+  let html = "";
+  for (let dayNo = 1; dayNo < 6; dayNo++) {
+    // let style = model.inputs.logPage.selectedDay == dayNo ? 'background-color: lightgreen' : '';
+    let style = "";
+    if (model.inputs.logPage.selectedDay == dayNo)
+      style = "background-color: lightgreen";
+    html += /*HTML*/ `
+            <button 
+                onclick="logPageSelectDay(${dayNo})"
+                style="${style}"
+                >
+                Dag ${dayNo}
+            </button>
+        `;
+  }
+  return html;
+}
+
+function createQuestionsHtml() {
+  let html = "";
+  for (
+    let questionIndex = 0;
+    questionIndex < model.mainQuestions.length;
+    questionIndex++
+  ) {
+    let question = model.mainQuestions[questionIndex];
+    let currentValue = model.inputs.logPage.answers[questionIndex] || 5;
+    let starsHtml = "";
+    for (let starNo = 1; starNo < 10; starNo++) {
+      starsHtml += /*HTML*/ `
+                <button                     
+                    onclick="selectStars(${questionIndex}, ${starNo})">
+                    ${starNo <= currentValue ? "★" : "☆"}
+                </button>
+            `;
+    }
+    html += /*HTML*/ `
+            <b>${question}</b><br/>
+            <div>
+                ${starsHtml} (${currentValue}/9)
+            </div>
+        `;
+  }
+  return html;
+}
+
+
+// if( sånn og sånn) {
+//     a = 1;
+// } else {
+//     a = 2;
+// }
+
+// a = sånn og sånn ? 1 : 2;
