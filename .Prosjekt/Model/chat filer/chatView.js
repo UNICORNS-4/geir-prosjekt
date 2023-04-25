2a727b467b64ab44765561735d92ce67fae3e014
Sandra <sandranordrum@hotmail.com>
2023-04-25 21:00:33 +0200

    chatView

--- .Prosjekt/Model/chat filer/chatView.js
+++ .Prosjekt/Model/chat filer/chatView.js
@@ -5,6 +5,7 @@ function updateChatView(){
     
     //finn alle meldinger som skal til den innlogga brukern
     let chatMessage = model.chatMessages.find(msg => msg.toUserId == model.users[i].userId);
+    let chatMessage1 = model.chatMessages.find(msg => msg.fromUserId == model.users[i].userId);
     if (chatMessage == null || undefined) continue;
 
     html += /*HTML*/ `
@@ -14,31 +15,27 @@ function updateChatView(){
         ${model.users[i].firstname} ${model.users[i].lastname}
       </ul>
     </div>
+
+    <div>Chat mellom deg og ${model.users[i].firstname} ${model.users[i].lastname}
+    <input oninput="${model.inputs.chatPage.newMessageInput} = this.value" type="text">
+    <button onclick="sendMld()">Send</button>
+    </div>
     `;
+    
   };
   return html;
 }
 
 function openChat(index){
   let chatMessage = model.chatMessages.find(msg => msg.toUserId == model.users[index].userId);
+  let chatMessage1 = model.chatMessages.find(msg => msg.fromUserId == model.users[index].userId);
   chatMessage.isRead = !chatMessage.isRead;
   updateView();
 }
 
-
-function chatBox(){
-  let html = "";
-  html += /*HTML*/ `
-  <div>Chat mellom deg og ${model.users[i].firstname} ${model.users[i].lastname}
-    <textarea cols="30" rows="10"></textarea>
-    <button onclick="sendMld()">Send</button>
-    </div>
-    `;
-}
-
 function sendMld(){
-  
-  model.users.push(model.inputs.newUserPage)
+  let loggedInUser = findUser(model.app.inputs.loginPage)
+  model.app.loggedInUser.inputs.chatPage.newMessageInput.user.userId.push(model.chatMessages[i])
 }
 
   
