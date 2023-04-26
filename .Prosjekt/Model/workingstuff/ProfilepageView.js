b3f78e99f06882cab9e2d3920c54afb9fabab687
MarlieJ <122234826+MarlieJ@users.noreply.github.com>
2023-04-26 21:57:24 +0200

    progressbar endret hardkoda

--- .Prosjekt/Model/workingstuff/ProfilepageView.js
+++ .Prosjekt/Model/workingstuff/ProfilepageView.js
@@ -25,31 +25,14 @@ function updateBars() {
   let html = /* html */ `
   <div class="ProfilePage-ProgressBars">
     <label for="bar1">Tema 1: Trygghet</label>
-    <progress value="${findProg()}" max ="${model.oppgaver[0,1,2,3]}"></progress>
-    <!--<progress id = "bar1" class="bars" value="4" max="4"></progress>-->
-    
+    <progress id = "bar1" class="bars" value="4" max="4"></progress>
     
     <label for="bar2">Tema 2: Growth mindset</label>
-    <progress value="${findProg()}" max ="${model.oppgaver[4,5,6,7]}"></progress>
-    <!--<progress id = "bar2" class="bars" value="12" max="100"></progress>-->
+    <progress id = "bar2" class="bars" value="75" max="100"></progress>
     
     <label for="bar3">Tema 3: Mikrovaner</label>
-    <progress value="${findProg()}" max ="${model.oppgaver[8,9,10,11]}"></progress>
-    <!--<progress id = "bar3" class="bars" value="76" max="100"></progress>-->
+    <progress id = "bar3" class="bars" value="0" max="100"></progress>
   </div>
 `
   return html;
 }
-
-/* function updateBarsWithIf(){
-  let html = "";
-  if(model.oppgaver[0,1,2,3] == "done"){
-    findProg() += 25
-  }
-  else if(model.oppgaver[4,5,6,7] == "done"){
-    findProg() += 25
-  }
-  else{
-    findProg() += 25
-  }
-} */
