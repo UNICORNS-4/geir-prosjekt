e4e8d347d841e385d2117eed84f7cea087088b32
MarlieJ <122234826+MarlieJ@users.noreply.github.com>
2023-04-26 15:44:43 +0200

    Jobbet med progressbars, få til å være dynamiske?

--- .Prosjekt/Model/workingstuff/ProfilepageView.js
+++ .Prosjekt/Model/workingstuff/ProfilepageView.js
@@ -5,9 +5,9 @@ let Progressbar3 = 0;
 function updateProfilepageView() {
   return /* Html */ `
   <div class="ProfilepageContainAll">
-  <div id="ProfilePic" ><img id="img1" src="img/${model.app.loggedInUser.imgSrc}"></div>
-  <div id="ProfileName"><b>${model.app.loggedInUser.firstname} ${model.app.loggedInUser.lastname}</b></div>
-  <div id="StartIT"><b>${model.app.loggedInUser.klasse}</b></div>
+  <div id="ProfilePic" ><img id="img1" src="img/${findUser(model.app.loggedInUser).imgSrc}"></div>
+  <div id="ProfileName"><b>${findUser(model.app.loggedInUser).firstname} ${findUser(model.app.loggedInUser).lastname}</b></div>
+  <div id="StartIT"><b>${findUser(model.app.loggedInUser).klasse}</b></div>
   <div id="Progressbars"></div>
   <div id="Bars">${updateBars()}</div>
   </div>
@@ -20,18 +20,36 @@ function updateBars() {
     model.app.loggedInUser.utførteOppgaver /
     model.app.loggedInUser.oppgaver /
     100;
-
+    
+    
   let html = /* html */ `
   <div class="ProfilePage-ProgressBars">
     <label for="bar1">Tema 1: Trygghet</label>
-    <progress id = "bar1" class="bars" value="4" max="4"></progress>
-
+    <progress value="${findProg()}" max ="${model.oppgaver[0,1,2,3]}"></progress>
+    <!--<progress id = "bar1" class="bars" value="4" max="4"></progress>-->
+    
+    
     <label for="bar2">Tema 2: Growth mindset</label>
-    <progress id = "bar2" class="bars" value="12" max="100"></progress>
+    <progress value="${findProg()}" max ="${model.oppgaver[4,5,6,7]}"></progress>
+    <!--<progress id = "bar2" class="bars" value="12" max="100"></progress>-->
     
     <label for="bar3">Tema 3: Mikrovaner</label>
-    <progress id = "bar3" class="bars" value="76" max="100"></progress>
+    <progress value="${findProg()}" max ="${model.oppgaver[8,9,10,11]}"></progress>
+    <!--<progress id = "bar3" class="bars" value="76" max="100"></progress>-->
   </div>
-`;
+`
   return html;
 }
+
+/* function updateBarsWithIf(){
+  let html = "";
+  if(model.oppgaver[0,1,2,3] == "done"){
+    findProg() += 25
+  }
+  else if(model.oppgaver[4,5,6,7] == "done"){
+    findProg() += 25
+  }
+  else{
+    findProg() += 25
+  }
+} */
